034079c9b8d019a2c7ffdab6fa8c5f7a5c748b7f
mr-abdellah <belkaid.abdulah@gmail.com>
2022-11-19 11:42:18 +0100

    Add navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import "./home.scss";
 import Sidebar from "./../components/Sidebar/Sidebar";
+import Navbar from "./../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="Home__container">
+        <Navbar />
         <h1>container</h1>
       </div>
     </div>
--- src/pages/components/Navbar/Navbar.jsx
+++ src/pages/components/Navbar/Navbar.jsx
@@ -1,9 +1,57 @@
-import './navbar.scss'
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
+
+// images
+import Profilepic from "../../../assets/Profilepic.jpg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search ..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
 
-export default Navbar
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" />
+          </div>
+
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+
+          <div className="item">
+            <FormatListBulletedOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src={Profilepic} alt="avatar" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
